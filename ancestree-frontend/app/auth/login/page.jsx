e94f1d01b7f25f98e3594676700bf5c3c33f43f7
Dione Alfred Cabigas <dionealfred.cabigas@cit.edu>
2025-05-16 13:10:33 +0800

    Redesigning and Removing some unrollbacked files

--- ancestree-frontend/app/auth/login/page.jsx
+++ ancestree-frontend/app/auth/login/page.jsx
@@ -1,20 +1,42 @@
 'use client'
 
 import Navbar from '../../../components/Navbar';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "@/app/utils/firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import AuthController from '@/components/AuthController';
+import Image from 'next/image';
 import Link from "next/link";
 
 function LoginContent() {
   const router = useRouter();
 
   const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+        setUser(currentUser);
+        
+        // If user is authenticated, fetch their data from Firestore
+        if (currentUser) {
+          try {
+            const userDoc = await getDoc(doc(db, "users", currentUser.uid));
+            if (userDoc.exists()) {
+              setUserData(userDoc.data());
+            }
+          } catch (error) {
+            console.error("Error fetching user data:", error);
+          }
+        }
+      });
+  
+      return () => unsubscribe();
+    }, []);
 
   const onLogin = async (event) => {
     event.preventDefault();
@@ -56,17 +78,24 @@ function LoginContent() {
     <div className="flex min-h-screen">
       {/* Left side - Login form */}
       <div className="w-full md:w-1/2 flex flex-col justify-center items-center bg-white px-4 md:px-8">
+        <Link href={user ? "/home" : "/"} className="flex items-center">
+          {/* Logo with dark accent background and white text */}
+          <Image src="/images/AncesTree_Logo.png" alt="Logo" width={135} height={40}/>
+        </Link>
         <div className="w-full max-w-sm">
   
           <h2 className="text-4xl font-bold text-center mb-1"><span className="text-[#365643]">WELCOME</span> BACK!</h2>
           <p className="text-sm text-gray-500 text-center mb-6">Ready to manage your Family Tree again?</p>
   
-          <form className="pt-6">
+          <form className="pt-6" onSubmit={onLogin}>
             <div className="mb-4">
               <label className="block text-gray-700 text-base mb-2">Email</label>
               <input
                 type="email"
+                id='email'
                 className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#365643]"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
@@ -75,7 +104,10 @@ function LoginContent() {
               <label className="block text-gray-700 text-base mb-2">Password</label>
               <input
                 type="password"
+                id='password'
                 className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#365643]"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
               />
             </div>
@@ -86,7 +118,7 @@ function LoginContent() {
   
             <button
               type="submit"
-              className="w-full bg-[#365643] hover:bg-green-700 text-white py-2 px-4 rounded-md"
+              className="w-full bg-[#365643] hover:bg-[#294032] text-white py-2 px-4 rounded-md"
             >
               LOGIN
             </button>
