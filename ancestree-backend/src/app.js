e976fc030fd967b7f7931df79067592628913637
Dialcab <dionealfred.cabigas@cit.edu>
2026-05-19 03:45:15 +0800

    Update app.js

--- ancestree-backend/src/app.js
+++ ancestree-backend/src/app.js
@@ -19,8 +19,8 @@ const importTreeRequestRoutes = require("./routes/importTreeRequestRoutes");
 const app = express();
 const port = process.env.PORT || 3001;
 
-// app.use(cors({ origin: "https://ancestree2025.netlify.app", credentials: true }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://ancestree2025.netlify.app", credentials: true }));
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(bodyParser.json());
 // app.use(express.json()); // Ignore this
 
